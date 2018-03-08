de45a3b80778ee500c81614842fa344b80df462b
Diego Borrero <die.9511@gmail.com>
2018-03-07 22:43:01 -0500

    Update, Delete and Create implemented

--- src/main/resources/static/js/RestaurantRestController.js
+++ src/main/resources/static/js/RestaurantRestController.js
@@ -2,24 +2,46 @@ var RestControllerModule = (function () {
 
     var getOrders = function (callback) {
         axios.get('/orders')
-        .then(function (ordersList){
-            callback.onSuccess(ordersList.data);
-        })
-        .catch(function (error){
-            callback.onFailed(error);
-        });
+            .then(function (ordersList) {
+                callback.onSuccess(ordersList.data);
+            })
+            .catch(function (error) {
+                callback.onFailed(error);
+            });
     };
 
     var updateOrder = function (order, callback) {
-    // todo implement
+        axios.put('/orders', order, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(function (response) {
+                callback.onSuccess(response);
+            })
+            .catch(function (error) {
+                callback.onFailed(error);
+            });
     };
 
     var deleteOrder = function (orderId, callback) {
-    // todo implement
+        axios.delete('/orders', orderId)
+            .then(function (response) {
+                callback.onSuccess(response);
+            })
+            .catch(function (error) {
+                callback.onFailed(error);
+            });
     };
 
     var createOrder = function (order, callback) {
-    // todo implement
+        axios.post('/orders', order)
+            .then(function (response) {
+                callback.onSuccess(response);
+            })
+            .catch(function (error) {
+                callback.onFailed(error);
+            });
     };
 
     return {
